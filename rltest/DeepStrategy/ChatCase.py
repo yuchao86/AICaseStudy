1353c3b5d2a23eb4d1c58fe8c64d8b2204ec9a5b
yuchao <yuchao86@gmail.com>
2025-05-29 14:28:17 +0800

    添加代码

--- rltest/DeepStrategy/ChatCase.py
+++ rltest/DeepStrategy/ChatCase.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     #Customer = namedtuple("Customer", "name sex age class address time buy order fidelity")
     myself = Customer("Yu Chao", "Y", 25, "三班", "北京海淀", 1500, 1, 101010, 123)
     dialog = [DialogItem(1, 0,"家长，你好，我能给你孩子提供一些学习上的帮助", 0.5),
-              DialogItem(2,1,"我们是名师天团著名的老师，有丰富的教学经验",  1.5),
+              DialogItem(2,1,"我们是著名的老师，有丰富的教学经验",  1.5),
               DialogItem(3,2,"你看孩子什么时间有空", 2.5),
               DialogItem(4,3,"孩子目前的成绩怎么样？",  5.0)]
 
